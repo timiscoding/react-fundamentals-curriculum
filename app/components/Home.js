1e6492652f3e96876be9f92e917ac6620756a2be
Timothy Tsui <timiscoding@gmail.com>
2016-04-10 14:48:19 +1000

    Home UI and CSS in external file

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,13 +4,27 @@ let styles = {
   container: {
     backgroundImage: 'url(../images/pattern.svg)',
     flex: '1',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white'
+  },
+  space: {
+    marginTop: '10px'
   }
 };
 
 class Home extends React.Component {
   render () {
     return (
-      <div style={styles.container}>Home</div>
+      <div style={styles.container}>
+        <h1>Enter a City and State</h1>
+        <div className="col-xs-2" style={styles.space}>
+          <input type="input" className="form-control" placeholder="Sydney, Australia" />
+        </div>
+        <button className="btn btn-lg btn-success" style={styles.space}>Get Weather</button>
+      </div>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+'use strict';
+
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let extractCSS = new ExtractTextPlugin('stylesheets/[name].css');
+
 module.exports = {
   entry:
     [
@@ -11,10 +17,14 @@ module.exports = {
     loaders:
       [
         { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-        { test: /\.css$/, loader: 'style-loader!css-loader' },
+        { test: /\.css$/, loader: extractCSS.extract(['css']) },
         { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
         { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
         { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
       ]
-  }
+  },
+  plugins:
+    [
+      extractCSS
+    ]
 };
